247ddc6ce143026b0cf099494ad98953a79ab97d
keleborn <Ivanenko-Aleksei@yandex.ru>
2024-07-09 14:33:08 +0300

    Update test-validation.ts

--- server/src/schema-validation/test-validation.ts
+++ server/src/schema-validation/test-validation.ts
@@ -10,7 +10,9 @@ console.log('Connected!\n');
 try {
     // eslint-disable-next-line
     await collections?.users?.insertOne(<any>{
-        age: 25
+        age: 25,
+        name: 'Alex',
+        isAdmin: false
     });
 }
 catch (error) {
